c766b8d8c793eb09b97402f99c2722285832f23e
sarisiza <sarita.iza@gmail.com>
2025-05-12 16:24:31 -0600

    Added logging dependencies

--- networkhelpers/build.gradle.kts
+++ networkhelpers/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
